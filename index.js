5db2155066fbe95e863913593407869094e0f604
asyncdoggo <ayushdeshpande81@gmail.com>
2023-02-19 15:11:31 +0530

    Add audio equilizer visualizer

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ let index = 0;
 let progress = document.getElementById("music_progress");
 let time = document.getElementById("time")
 
+var canvas,
+    ctx,
+    source,
+    context,
+    analyser,
+    fbc_array,
+    bar_count,
+    bar_pos,
+    bar_width,
+    bar_height;
+
+
+    
 
 
 
@@ -47,6 +60,23 @@ function Play() {
         audio.pause();
         document.getElementById("play").innerHTML = "play_circle";
     }
+
+
+    if (typeof(context) === "undefined") {
+        context = new AudioContext();
+        analyser = context.createAnalyser();
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        source = context.createMediaElementSource(audio);
+
+        // canvas.width = window.innerWidth * 0.80;
+        // canvas.height = window.innerHeight * 0.60;
+
+        source.connect(analyser);
+        analyser.connect(context.destination);
+    }
+    
+    FrameLooper();
 }
 
 // Function to update audioList on loading audio files
@@ -112,3 +142,27 @@ document.getElementById("next").addEventListener("click",next)
 
 document.getElementById("previous").addEventListener("click",previous)
 
+
+function FrameLooper() {
+    window.RequestAnimationFrame =
+        window.requestAnimationFrame(FrameLooper) ||
+        window.msRequestAnimationFrame(FrameLooper) ||
+        window.mozRequestAnimationFrame(FrameLooper) ||
+        window.webkitRequestAnimationFrame(FrameLooper);
+
+    fbc_array = new Uint8Array(analyser.frequencyBinCount);
+    bar_count = window.innerWidth / 2;
+
+    analyser.getByteFrequencyData(fbc_array);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#ffffff";
+
+    for (var i = 0; i < bar_count; i++) {
+        bar_pos = i * 4;
+        bar_width = 2;
+        bar_height = -(fbc_array[i] / 2);
+
+        ctx.fillRect(bar_pos, canvas.height, bar_width, bar_height);
+    }
+}
